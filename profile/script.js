14f035302a78c4993c1cbd196e7b276a165e0045
skhoulan <samir_khoulani@student.uml.edu>
2016-12-16 02:22:07 -0500

    first major update

---
+++ profile/script.js
@@ -0,0 +1,34 @@
+function revealFortune() {
+
+    var d = new Date();
+
+    //Here I am subtracting the current time in milliseconds from the time that the fortune button was clicked. I have hard coded that number because manuall setting a Date object was bugged for some reason.
+    var quoteNum = Math.floor(Math.abs((d.getTime() - 1480806021656)/84600000)) % 318; // I mod by the number of fortunes I have in the text file
+
+    var txtFile = new XMLHttpRequest();
+    txtFile.open("GET", "https://skhoulani.github.io/HBC/fortune.txt", true);
+    txtFile.onreadystatechange = function()
+    {
+      if (txtFile.readyState === 4) {  // document is ready to parse.
+        if (txtFile.status === 200) {  // file is found
+          allText = txtFile.responseText;
+          lines = txtFile.responseText.split("\n");
+
+          var fortuneDiv = document.getElementById("fortuneDiv");
+
+          fortuneDiv.removeChild(fortuneDiv.childNodes[3]);
+
+          var node = document.createElement('em');
+          var textnode = document.createTextNode(lines[quoteNum]);
+          node.appendChild(textnode);
+          fortuneDiv.appendChild(node);
+
+          document.getElementsByTagName("em")[0].style.fontSize = "50px";
+          document.getElementsByTagName("em")[0].style.color = "#E4FED1";
+
+
+        }
+      }
+    }
+    txtFile.send(null);
+};
--- reminders/script.js
+++ reminders/script.js
@@ -1,13 +1,15 @@
-$("#inputReminder").keypress(function(event) {
-    //$("#submitButton").on("click", function(e) {
-        event.preventDefault();
-        var keycode = (event.keyCode ? event.keyCode : event.which);
-        if(event.keyCode == '13'){
-            console.log('You pressed an "enter" key in somewhere');
-        }
-    });
+//This JQuery isn't working for some strange reason
+// $(".form-group #inputReminder").on("keyup", function(event) {
+//     //$("#submitButton").on("click", function(e) {
+//         event.preventDefault();
+//         //var keycode = (event.keyCode ? event.keyCode : event.which);
+//         console.log(event);
+//         if(event.keyCode == 13){
+//             console.log('You pressed an "enter" key in somewhere');
+//         }
+//     });
 
-
+//another test with Jquery also not working
 //         //$("#checklist").after("<br>");
 //     });
 //     $(document).bind("enterKey",function(e){
@@ -21,7 +23,60 @@ $("#inputReminder").keypress(function(event) {
 //         }
 //     })
 // });
-// function isEnter(event) {
-//     if (event.keyCode == '13')
-//         console.log("You pressed a key inside the input field");
-// }
+// $(document).ready(function() {
+// // Keypress does not work here as the character needs to be written to screen before
+// // the event handler fires.
+//
+//
+//     $("#inputReminder").keyup(function(event){
+//         console.log(event.which)
+//
+//
+//         if (event.which == 13) {
+//             return false;
+//             console.log("hey");
+//
+//         }
+//     })
+// })
+
+//Prevents the enter button from submitting a form
+var i = 0;
+function noenter1(e){
+    //removes last checklist item each time a character is entered on keybaord
+    //$("#checklist").last().remove();
+    if (e.which == 13) {
+        $("#incomplete").append("<li ><label id=check'"+ i +"'>" + $("#inputReminder1").val() + "</label></li>");
+        $("#inputReminder1").val("");
+        i++;
+    }
+
+    return !(window.event && window.event.keyCode == 13);
+
+
+}
+
+var j = 0;
+
+function noenter2(e){
+    //removes last checklist item each time a character is entered on keybaord
+    //$("#checklist").last().remove();
+    if (e.which == 13) {
+        $("#complete").append("<li ><label id=check'"+ j +"'>" + $("#inputReminder2").val() + "</label></li>");
+        $("#inputReminder2").val("");
+        j++;
+    }
+
+    return !(window.event && window.event.keyCode == 13);
+
+
+}
+
+ var numChecked = 0;
+
+ $('.checkbox input[type=checkbox]').change(function(event) {
+     event.preventDefault();
+     console.log("hi")
+     numChecked = $(".checkbox input[type=checkbox]").checked.length;
+
+ });
